a8db44e42296ce36664b64229ca0fb8d7fa6ccfe
Thulio Philipe <thulioph@gmail.com>
2017-12-15 14:42:51 -0300

    Updates the resume component to just render content.

--- src/components/resume/index.js
+++ src/components/resume/index.js
@@ -1,20 +1,10 @@
 import React from 'react';
-import Records from '../../utils/records';
 
 // ====
 
 class Resume extends React.Component {
-    constructor(props) {
-        super(props);
-        
-        this.records = new Records();
-    }
-
     render() {
-        const { 
-            allRecords, allMayorEntry, 
-            allViceMayorEntry, allPaidValue
-        } = this.props;
+        const { boxOne, boxTwo, boxTree, boxFour } = this.props;
 
         return (
             <section className="hero is-dark is-bold">
@@ -23,33 +13,29 @@ class Resume extends React.Component {
                         <nav className="level">
                             <div className="level-item has-text-centered">
                                 <div>
-                                    <p className="heading">Movimentações</p>
-                                    <p className="title">
-                                        {allRecords.length}
-                                    </p>
+                                    <p className="heading">{boxOne.title}</p>
+                                    <p className="title">{boxOne.amount}</p>
                                 </div>
                             </div>
 
                             <div className="level-item has-text-centered">
                                 <div>
-                                    <p className="heading">Gasto anual</p>
-                                    <p className="title">
-                                        {this.records.buildPaidValue(allPaidValue)}
-                                    </p>
+                                    <p className="heading">{boxTwo.title}</p>
+                                    <p className="title">{boxTwo.amount}</p>
                                 </div>
                             </div>
 
                             <div className="level-item has-text-centered">
                                 <div>
-                                    <p className="heading">Transações do Prefeito</p>
-                                    <p className="title">{allMayorEntry.length}</p>
+                                    <p className="heading">{boxTree.title}</p>
+                                    <p className="title">{boxTree.amount}</p>
                                 </div>
                             </div>
 
                             <div className="level-item has-text-centered">
                                 <div>
-                                    <p className="heading">Transações do Vice Prefeito</p>
-                                    <p className="title">{allViceMayorEntry.length}</p>
+                                    <p className="heading">{boxFour.title}</p>
+                                    <p className="title">{boxFour.amount}</p>
                                 </div>
                             </div>
                         </nav>
