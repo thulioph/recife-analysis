5f0feacfaa8c8c8d6eae51fa39295c624c37e0d8
thulioph <thulioph@gmail.com>
2017-12-20 23:36:50 -0300

    Small refactor on the graphic bar.

--- src/components/graphic/bar.js
+++ src/components/graphic/bar.js
@@ -37,8 +37,17 @@ class GraphicBar extends React.Component {
         );
     }
 
+    buildHeightBar(value, y) {
+        if (value !== 0 && Math.sign(value) === 1) {
+            return height - y(value);
+        } else {
+            // console.warn('diferente', value);
+            return height - y(Math.abs(value));
+        }
+    }
+
     buildRecordsGraphic(arr, viz, x, y) {
-        x.domain(arr.map((obj) => obj.mes_movimentacao));
+        x.domain(arr.map((obj) => obj.mes_movimentacao).sort((a, b) => a - b));
         y.domain([0, d3.max(arr, ((obj) => parseInt(obj.valor_pago, 10)))]);
 
         // add the rectangles
@@ -46,16 +55,16 @@ class GraphicBar extends React.Component {
             .remove().exit() // clear the last data
             .data(arr).enter() // call the new data
             .append('rect')
-            .attr('class', 'bar')
-            .attr('x', (obj) => x(obj.mes_movimentacao))
-            .attr('width', x.bandwidth())
-            .attr('y', (obj) => y(parseInt(obj.valor_pago, 10)))
-            .attr('height', (obj) => this.buildHeightBar(parseInt(obj.valor_pago, 10), y))
-            .style('fill', (obj) => {
-                if (parseInt(obj.valor_pago, 10) <= 0) {
-                    return '#F00'; // negative
-                }
-            })
+                .attr('class', 'bar')
+                .attr('x', (obj) => x(obj.mes_movimentacao))
+                .attr('width', x.bandwidth())
+                .attr('y', (obj) => y(parseInt(obj.valor_pago, 10)))
+                .attr('height', (obj) => this.buildHeightBar(parseInt(obj.valor_pago, 10), y))
+                .style('fill', (obj) => {
+                    if (parseInt(obj.valor_pago, 10) <= 0) {
+                        return '#F00'; // negative
+                    }
+                })
             .on('click', (obj) => this.displayMoreInformation(obj));
 
         // add the xAxis
@@ -65,28 +74,16 @@ class GraphicBar extends React.Component {
         viz.append('g').call(d3.axisLeft(y).ticks(10));
     }
 
-    buildHeightBar(value, y) {
-        if (value !== 0 && Math.sign(value) === 1) {
-            return height - y(value);
-        } else {
-            // console.warn('diferente', value);
-            return y(Math.abs(value));
-        }
-    }
-
-    createCharBar() {
-        const x = d3.scaleBand().range([0, width]).paddingInner(0.1);
+    createCharBar(data) {
+        const x = d3.scaleBand().range([0, width]).paddingInner(0.2);
         const y = d3.scaleLinear().range([height, 0]);
 
         const viz = d3.select(this.node);
 
-        viz
-            .attr('width', width + this.margin.left + this.margin.right)
+        viz.attr('width', width + this.margin.left + this.margin.right)
             .attr('height', height + this.margin.top + this.margin.bottom)
             .append('g')
-                .attr('transform', "translate(" + this.margin.left + "," + this.margin.top + ")");
-
-        const { data } = this.props;
+            .attr('transform', "translate(" + this.margin.left + "," + this.margin.top + ")");
 
         // avoid the graphic to render many times getting the size of array
         if (data.length > 99) {
@@ -95,11 +92,13 @@ class GraphicBar extends React.Component {
     }
 
     componentDidMount() {
-        this.createCharBar();
+        const { data } = this.props;
+        this.createCharBar(data);
     }
     
     componentDidUpdate() {
-        this.createCharBar();
+        const { data } = this.props;
+        this.createCharBar(data);
     }
 
     render() {
